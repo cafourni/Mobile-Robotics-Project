cf246a95c1e4192796353db6e140c51f191dbb7a
Eaellen <150853101+Eaellen@users.noreply.github.com>
2023-12-03 19:45:22 +0100

    Add files via upload

--- cvision2.py
+++ cvision2.py
@@ -1,6 +1,5 @@
 import cv2
 from cv2 import aruco
-
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -59,19 +58,6 @@ class Obj:
         self.id = id
         self.img_pos = img_pos
         self.corners = ()
-
-    """ def detect(self,frame,detector):
-        corners, _, _ = detector.detectMarkers(frame, ids = cv2.UMat(np.array([self.id])))
-        if np.size(corners,0) > 0:
-            centers = []
-            for i in range(np.size(corners,0)):
-                print('Corners :', corners[i].get())
-                centers.append(get_center(corners[i].get()))
-                print('Center i :', get_center(corners[i].get()))
-            print('Centers :', corners)
-            self.img_pos = np.array(centers)
-        self.corners = corners
-        return"""
     
     def get_img_dir(self):
         img_dir = []
@@ -96,7 +82,7 @@ class Obj:
 # Class Map define a map given three references objects and their respective distances. 
 # This map can be used to localize other objects given a frame of the actual view.
 class Map:
-    def __init__(self, ref1, ref2, ref3, distance_r1r2 = 650, distance_r3r2 = 600):
+    def __init__(self, ref1, ref2, ref3, distance_r1r2, distance_r3r2):
         self.ref1 = ref1
         self.ref1_pos = np.array([1, 0])
         self.ref2 = ref2
@@ -110,7 +96,7 @@ class Map:
         self.e2 = (self.ref3_pos - self.ref2_pos)/np.linalg.norm(self.ref3_pos - self.ref2_pos)
         self.scale = np.array([distance_r1r2/np.linalg.norm(self.ref1_pos - self.ref2_pos), distance_r3r2/np.linalg.norm(self.ref3_pos - self.ref2_pos)])
         
-    def loc_img2map(self, frame, img_coordinates):
+    def loc_img2map(self, img_coordinates):
         if np.size(img_coordinates) > 1:
             r1_img_pos = self.ref1.img_pos[0]
             r2_img_pos = self.ref2.img_pos[0]
@@ -128,7 +114,7 @@ class Map:
         else:
             return np.array([])
     
-    def vec_img2map(self, frame, img_coordinates):
+    def vec_img2map(self, img_coordinates):
         if np.size(img_coordinates) > 1:
             r1_img_pos = self.ref1.img_pos[0]
             r2_img_pos = self.ref2.img_pos[0]
@@ -146,7 +132,7 @@ class Map:
         else:
             return np.array([])
     
-    def loc_map2img(self, frame, map_coordinates):
+    def loc_map2img(self, map_coordinates):
         if np.size(map_coordinates) > 1:
             map_coordinates = np.divide(map_coordinates, self.scale)
             r1_img_pos = self.ref1.img_pos[0]
@@ -159,98 +145,55 @@ class Map:
         else:
             return np.array([])
     
-    def localize(self, frame, obj):
+    def localize(self, obj):
         img_coordinates = obj.img_pos
         map_pos = []
         for o in range(np.size(img_coordinates,0)):
-            map_pos.append(self.loc_img2map(frame, img_coordinates[o]))
+            map_pos.append(self.loc_img2map(img_coordinates[o]))
         return np.array(map_pos)
     
-    def matrix(self, frame, objects, robot, goal):
+    def matrix(self, frame, robot, goal):
         # Print the map in a matrix format with the given objects represented as numbers
-        square_size = 100 #[mm]
+        square_size = 10 #[mm]
         margins = 0
         origin = np.array([margins, margins])
-        matrix = np.zeros([2*margins+self.distance_r1r2//square_size,2*margins+self.distance_r1r2//square_size])
+        matrix = np.zeros([2*margins+self.distance_r1r2//square_size,2*margins+self.distance_r3r2//square_size])
         # Write the obstacles positions in the matrix with a 1
-        for o in range(len(objects)):
-            map_pos = self.localize(frame,objects[o])
-            for p in range(np.size(map_pos,0)):
-                if np.size(map_pos[p]) > 0:
-                    if not(np.isnan(map_pos[p]).any()):
-                        mat_pos = origin + np.round(np.divide(map_pos[p],square_size)).astype(int)
-                        if mat_pos[0] > 0 and mat_pos[1] > 0 and mat_pos[0] < np.size(matrix,0) and mat_pos[1] < np.size(matrix,1):
-                            matrix[mat_pos[0],mat_pos[1]] = 1
-                        if np.size(objects[o].boundingRect[p]) > 0:
-                            x,y,w,h = objects[o].boundingRect[p]
-                            left_top_corner = self.loc_img2map(frame,np.array([x,y]))
-                            right_bottom_corner = self.loc_img2map(frame,np.array([x+w,y+h]))
-                            mat_l_t_c = origin + np.round(np.multiply(left_top_corner, np.divide(self.scale,square_size))).astype(int)
-                            mat_r_b_c = origin + np.round(np.multiply(right_bottom_corner, np.divide(self.scale,square_size))).astype(int)
-                            if mat_l_t_c[0] > 0 and mat_l_t_c[1] > 0 and mat_l_t_c[0] < np.size(matrix,0) and mat_l_t_c[1] < np.size(matrix,1):
-                                if mat_r_b_c[0] > np.size(matrix,0):
-                                    mat_r_b_c[0] = np.size(matrix,0)-1
-                                if mat_r_b_c[1] > np.size(matrix,1):
-                                    mat_r_b_c[1] = np.size(matrix,1)-1
-                                if mat_r_b_c[0] < 0:
-                                    mat_r_b_c[0] = 0
-                                if mat_r_b_c[1] < 0:
-                                    mat_r_b_c[1] = 0
-                                if mat_l_t_c[0] > mat_r_b_c[0]:
-                                    tmp = mat_l_t_c[0]
-                                    mat_l_t_c[0] = mat_r_b_c[0]
-                                    mat_r_b_c[0] = tmp
-                                if mat_l_t_c[1] > mat_r_b_c[1]:
-                                    tmp = mat_l_t_c[1]
-                                    mat_l_t_c[1] = mat_r_b_c[1]
-                                    mat_r_b_c[1] = tmp
-                                matrix[mat_l_t_c[0]:mat_r_b_c[0], mat_l_t_c[1]:mat_r_b_c[1]] = np.ones([abs(mat_r_b_c[0]-mat_l_t_c[0]),abs(mat_r_b_c[1]-mat_l_t_c[1])])
-                            else:
-                                if mat_r_b_c[0] > 0 and mat_r_b_c[1] > 0 and mat_r_b_c[0] < np.size(matrix,0) and mat_r_b_c[1] < np.size(matrix,1):
-                                    if mat_l_t_c[0] < 0:
-                                        mat_l_t_c[0] = 0
-                                    if mat_l_t_c[1] < 0:
-                                        mat_l_t_c[1] = 0
-                                    if mat_l_t_c[0] > np.size(matrix,0):
-                                        mat_l_t_c[0] = np.size(matrix,0)-1
-                                    if mat_l_t_c[1] > np.size(matrix,1):
-                                        mat_l_t_c[1] = np.size(matrix,1)-1
-                                    if mat_l_t_c[0] > mat_r_b_c[0]:
-                                        tmp = mat_l_t_c[0]
-                                        mat_l_t_c[0] = mat_r_b_c[0]
-                                        mat_r_b_c[0] = tmp
-                                    if mat_l_t_c[1] > mat_r_b_c[1]:
-                                        tmp = mat_l_t_c[1]
-                                        mat_l_t_c[1] = mat_r_b_c[1]
-                                        mat_r_b_c[1] = tmp
-                                    matrix[mat_l_t_c[0]:mat_r_b_c[0], mat_l_t_c[1]:mat_r_b_c[1]] = np.ones([abs(mat_r_b_c[0]-mat_l_t_c[0]), abs(mat_r_b_c[1]-mat_l_t_c[1])])
+        color_obstacle = cv2.imread('obstacle.jpg')
+        lower_b, upper_b = upper_lower_color(color_obstacle, 10)
+        # Find contours in the binary mask
+        mask = cv2.inRange(frame, lower_b, upper_b)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Create a matrix to store contour points
+        contour_matrix = np.multiply(np.ones_like(frame),255)
+        # Iterate through the contours and draw them on the matrix
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            # Adjust the area threshold based on your specific case
+            if area > 1000:
+                # Draw the contour on the matrix
+                cv2.drawContours(contour_matrix, [contour], -1, 0, thickness=cv2.FILLED)
+        show_frame(contour_matrix)
+        for x in range(2*margins+self.distance_r1r2//square_size):
+            for y in range(2*margins+self.distance_r3r2//square_size):
+                loc_img = self.loc_map2img([square_size*x,square_size*y])
+                if contour_matrix[loc_img[1], loc_img[0]].all() < 1:
+                    matrix[x,y] = 1
         # Write the robot position in the matrix with a 2
-        map_pos = self.localize(frame,robot)
+        map_pos = self.localize(robot)
         if np.size(map_pos,0) > 0:
             if not(np.isnan(map_pos[0]).any()):
                 mat_pos = origin + np.round(np.divide(map_pos[0],square_size)).astype(int)
                 if mat_pos[0] > 0 and mat_pos[1] > 0 and mat_pos[0] < np.size(matrix,0) and mat_pos[1] < np.size(matrix,1):
                     matrix[mat_pos[0],mat_pos[1]] = 2
         # Write the goal position in the matrix with a 3
-        map_pos = self.localize(frame,goal)
+        map_pos = self.localize(goal)
         if np.size(map_pos,0) > 0:
             if not(np.isnan(map_pos[0]).any()):
                 mat_pos = origin + np.round(np.divide(map_pos[0],square_size)).astype(int)
                 if mat_pos[0] > 0 and mat_pos[1] > 0 and mat_pos[0] < np.size(matrix,0) and mat_pos[1] < np.size(matrix,1):
                     matrix[mat_pos[0],mat_pos[1]] = 3
-            else:
-                print('Empty pos')    
-        else:
-            print('No goal')                    
-        """# Write the ref positions in the matrix with a 4
-        for o in range(len(refs)):
-            map_pos = self.localize(frame,refs[o])
-            for p in range(np.size(map_pos,0)):
-                if np.size(map_pos[p]) > 0:
-                    if not(np.isnan(map_pos[p]).any()):
-                        mat_pos = origin + np.round(np.divide(map_pos,square_size)).astype(int)
-                        if mat_pos[0] > 0 and mat_pos[1] > 0 and mat_pos[0] < np.size(matrix,0) and mat_pos[1] < np.size(matrix,1):
-                            matrix[mat_pos[0],mat_pos[1]] = 4 """
+
         return matrix
 
 
