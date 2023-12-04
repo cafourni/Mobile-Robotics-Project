40e19521cf6a69654dd01330905d17ec5b62df1f
Eaellen <150853101+Eaellen@users.noreply.github.com>
2023-12-04 11:54:24 +0100

    Add files via upload

--- cvision2.py
+++ cvision2.py
@@ -8,6 +8,8 @@ from IPython.display import display, clear_output
 import time
 import math
 
+############################################################################################################################################
+
 # Find the USB camera connected to the PC
 def find_camera_index(i):
     cap = cv2.Videocapture(i)
@@ -19,23 +21,41 @@ def find_camera_index(i):
         print(f"No camera found at index {i}.")
         return find_camera_index(i+1)
 
+############################################################################################################################################
+
 #
 def show_frame(frame):
     _, img_encoded = cv2.imencode('.png', frame)
     IPython.display.display(IPython.display.Image(data=img_encoded.tobytes()))
 
+############################################################################################################################################
+
 #
 def vector_to_angle(vector):
-    angle = -math.atan2(vector[1], vector[0])
-    return angle
+    if np.size(vector) == 2:
+        angle = -math.atan2(vector[1], vector[0])
+        return angle
+    else:
+        return None
 
-# Define the upper and lower RGB color limit of a given image 
+############################################################################################################################################
+
+############################################################################################################################################
+# Color recognition
+############################################################################################################################################
+
+############################################################################################################################################
+
+# Given an image, calculates the mean and the standard deviation of the color 
+# of all the pixels and return the upper and lower bound colors.
 def upper_lower_color(color_sample, c):
     # Define the upper and lower limit of the color given the sample image 
     mean, stddev = cv2.meanStdDev(color_sample)
     lower_color = mean - c*stddev
     upper_color = mean + c*stddev
     return lower_color, upper_color
+
+############################################################################################################################################
 
 # Given a frame, give the contour of all pixels in the image of a particular color.
 def find_contours(frame, color, c):
@@ -45,6 +65,15 @@ def find_contours(frame, color, c):
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
+############################################################################################################################################
+
+############################################################################################################################################
+# ArUco marker recognition
+############################################################################################################################################
+
+############################################################################################################################################
+# Given an array of pixel coordinates (for example the 4 corners of an ArUco marker),
+# return the centroid.
 def get_center(corners):
     # Get the pixel coordinates of the corners of the marker
     marker_corners = np.int32(corners)
@@ -52,7 +81,65 @@ def get_center(corners):
     centroid = np.round(np.mean(marker_corners, axis=0, dtype=np.int32)).astype(int)
     return centroid
 
-# Class Obj defining all the objects to be recognized with the camera.
+############################################################################################################################################
+# Create an ArucoDetector given a specific dictionary
+def create_detector(dictionary):
+    # Create a detector
+    parameters = cv2.aruco.DetectorParameters()
+    parameters.adaptiveThreshWinSizeMin = 10
+    parameters.adaptiveThreshWinSizeMax = 100
+    parameters.adaptiveThreshWinSizeStep = 10
+    """ parameters.minMarkerPerimeterRate = 0.03
+    parameters.maxMarkerPerimeterRate = 4.0
+    parameters.polygonalApproxAccuracyRate = 0.05
+    parameters.minCornerDistanceRate = 0.05
+    parameters.minDistanceToBorder = 3
+    parameters.minMarkerDistanceRate = 0.05
+    parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
+    parameters.cornerRefinementWinSize = 5
+    parameters.cornerRefinementMaxIterations = 30
+    parameters.cornerRefinementMinAccuracy = 0.1
+    parameters.markerBorderBits = 1
+    parameters.perspectiveRemovePixelPerCell = 4
+    parameters.perspectiveRemoveIgnoredMarginPerCell = 0.13
+    parameters.maxErroneousBitsInBorderRate = 0.04
+    parameters.minOtsuStdDev = 60.0
+    parameters.errorCorrectionRate = 0.6 """
+    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+    return detector
+
+############################################################################################################################################
+# Detect given objects on a given frame with a given detector. 
+# Update the image positions and corners of the objects. 
+def detect_objects(frame, detector, objects):
+    corners = np.array([[]])
+    corners, ids, _ = detector.detectMarkers(frame)
+    for o in range(np.size(objects,0)):
+        centers = []
+        c = []
+        if np.size(corners,0) > 0:
+            for i in range(np.size(ids,0)):
+                if objects[o].id == ids[i][0]:
+                    centers.append(get_center(corners[i][0]))
+                    c.append(corners[i][0])
+        objects[o].img_pos = np.array(centers)
+        objects[o].corners = np.array(c)
+    return
+
+############################################################################################################################################
+
+############################################################################################################################################
+# Classes
+############################################################################################################################################
+
+############################################################################################################################################
+# Class Obj :
+# Defining all the objects to be recognized with the camera. 
+# Attributes :  id - the id of the ArUco marker assigned to this object, 
+#               img_pos - position in pixels on the last image analysed 
+#                           (array of empty array of if none detected, array of arrays if several detected)
+#               corners - position of the 4 corners of the ArUco marker 
+#                           (empty if none detected, array of arrays if several detected)
 class Obj:
     def __init__(self, id, img_pos = np.array([[]])):
         self.id = id
@@ -78,9 +165,11 @@ class Obj:
                 cv2.circle(frame, self.img_pos[p], 5, color, -1)
         return
         
-
-# Class Map define a map given three references objects and their respective distances. 
-# This map can be used to localize other objects given a frame of the actual view.
+############################################################################################################################################
+# Class Map :
+# Define a map given three references objects and their respective distances. 
+# This map can be used to localize other objects given their image position of the actual view.
+# Before use, check that references are detected !
 class Map:
     def __init__(self, ref1, ref2, ref3, distance_r1r2, distance_r3r2):
         self.ref1 = ref1
@@ -95,7 +184,14 @@ class Map:
         self.e1 = (self.ref1_pos - self.ref2_pos)/np.linalg.norm(self.ref1_pos - self.ref2_pos)
         self.e2 = (self.ref3_pos - self.ref2_pos)/np.linalg.norm(self.ref3_pos - self.ref2_pos)
         self.scale = np.array([distance_r1r2/np.linalg.norm(self.ref1_pos - self.ref2_pos), distance_r3r2/np.linalg.norm(self.ref3_pos - self.ref2_pos)])
-        
+    
+    def references_detected(self):
+        if (np.size(self.ref1.img_pos,0) > 0) and (np.size(self.ref2.img_pos,0) > 0) and (np.size(self.ref3.img_pos,0) > 0):
+            # The three references needed to use the map are detected on the image.
+            return True
+        else:
+            return False
+
     def loc_img2map(self, img_coordinates):
         if np.size(img_coordinates) > 1:
             r1_img_pos = self.ref1.img_pos[0]
@@ -173,12 +269,14 @@ class Map:
             if area > 1000:
                 # Draw the contour on the matrix
                 cv2.drawContours(contour_matrix, [contour], -1, 0, thickness=cv2.FILLED)
-        show_frame(contour_matrix)
+        #show_frame(contour_matrix)
+        max_y, max_x, _ = contour_matrix.shape
         for x in range(2*margins+self.distance_r1r2//square_size):
             for y in range(2*margins+self.distance_r3r2//square_size):
                 loc_img = self.loc_map2img([square_size*x,square_size*y])
-                if contour_matrix[loc_img[1], loc_img[0]].all() < 1:
-                    matrix[x,y] = 1
+                if max_x > loc_img[0] and max_y > loc_img[1]:
+                    if contour_matrix[loc_img[1], loc_img[0]].all() < 1:
+                        matrix[x,y] = 1
         # Write the robot position in the matrix with a 2
         map_pos = self.localize(robot)
         if np.size(map_pos,0) > 0:
@@ -196,6 +294,5 @@ class Map:
 
         return matrix
 
-
-    
+############################################################################################################################################
 
